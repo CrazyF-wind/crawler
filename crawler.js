a23ed8002a5f03ccfc42dc1bfa3711544b7be6ce
Fwind <ff1990@126.com>
2016-10-25 14:15:38 +0800

    整理合并

--- crawler.js
+++ crawler.js
@@ -1,27 +1,23 @@
-var request=require("request");
-var cheerio=require("cheerio");
-var fs=require("fs");
+var request = require("request");
+var cheerio = require("cheerio");
+var fs = require("fs");
 
 request({
-// url:"http://www.acfun.tv/v/list110/index.htm",
-url:"http://www.csdn.net/",
-methed:"get"
-},function(e,r,b){
-if(e||!b){
-return;
-} 
+    // url:"http://www.acfun.tv/v/list110/index.htm",
+    url: "http://www.csdn.net/",
+    methed: "get"
+}, function (e, r, b) {
+    if (e || !b) {
+        return;
+    }
 
-var $=cheerio.load(b); 
-var result=[];
-// var titles=$(".mainer .item a.title");
-// var titles=$(".content clearfix .main fl .news clearfix .news_list ul li a.title");
-var titles=$(".news_list ul li a"); 
-for(var i=0;i<titles.length;i++){
-result.push($(titles[i]).text());
-console.log(JSON.stringify($(titles[i]).text())+"|"+JSON.stringify($(titles[i]).attr("href"))); 
-
-}
- 
-fs.writeFileSync("result.json",JSON.stringify(result));
-// console.log($);
+    var $ = cheerio.load(b);
+    var result = [];
+    var titles = $(".news_list ul li a");
+    console.log("csdn:头条！！")
+    for (var i = 0; i < titles.length; i++) {
+        result.push($(titles[i]).text());
+        console.log(JSON.stringify($(titles[i]).text()) + "|" + JSON.stringify($(titles[i]).attr("href")));
+    }
+    fs.writeFileSync("result.json", JSON.stringify(result));
 })
--- weather.js
+++ weather.js
@@ -4,8 +4,8 @@ var fs = require("fs");
 
 request({
     // url:"http://www.acfun.tv/v/list110/index.htm",
-    // url: "http://www.weather.com.cn/weather/101030100.shtml",
-    url: "http://www.csdn.net",
+     url: "http://www.weather.com.cn/weather/101030100.shtml",
+    //url: "http://www.csdn.net",
     methed: "get"
 }, function (e, r, b) {
     if (e || !b) {
@@ -13,17 +13,16 @@ request({
     }
 
     var $ = cheerio.load(b);
-    console.log(JSON.stringify($("div a").text()));
     var result = [];
     var titles = $("#7d ul.t li");
     for (var i = 0; i < titles.length; i++) {
-        console.log(JSON.stringify($(titles[i]).children("h1").text()) + "|" +
-            JSON.stringify($(titles[i]).children("p.wea").text()) + "|" +
+        console.log(JSON.stringify($(titles[i]).children("h1").text()) + " " +
+            JSON.stringify($(titles[i]).children("p.wea").text()) + " " +
             JSON.stringify($(titles[i]).children("p.tem").children("span").text()) + "/" +
-            JSON.stringify($(titles[i]).children("p.tem").children("i").text()) + "|" +
+            JSON.stringify($(titles[i]).children("p.tem").children("i").text()) + " " +
             JSON.stringify($(titles[i]).children("p.win").children("i").text())
         );
-        console.log("temp:" + ($(titles[i]).text()).replace(/[\n]/g, ""));
+        //console.log("temp:" + ($(titles[i]).text()).replace(/[\n]/g, ""));
     }
-    console.log(("\n24/15℃\n").replace("\n", ""));
+    //console.log(("\n24/15℃\n").replace("\n", ""));
 })
